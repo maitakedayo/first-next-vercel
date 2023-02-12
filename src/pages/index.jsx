9e7c8e42016dd4ed3d7531fc1ee2702cdb7df53f
aida mai <1100maimai@gmail.com>
2023-02-12 14:48:00 +0900

    second push

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Next.jsのVercelへのデプロイ完了！</h1>;
+  return <h1 className="bg-red-200">Next.jsのVercelへのデプロイ完了！</h1>;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    //"./src/pages/**/*.stories.@(js|jsx|ts|tsx)",
+    //"./src/stories/**/*.(js|jsx|ts|tsx)",
+    //"./src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
